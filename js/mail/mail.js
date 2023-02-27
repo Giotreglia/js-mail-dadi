ba7601d41047a86dad01da884631e0cd604cc254
Giotreglia <giovanni.treglia@gmail.com>
2023-02-27 14:52:08 +0100

    Verifica email corretta

---
+++ js/mail/mail.js
@@ -0,0 +1,28 @@
+/*
+    Chiedo all'utente mail
+    Fare controllo presenza mail inserita nella lista mail autorizzate
+    Stampare esito controllo
+    Se mail presente continua
+    Se mail non presente ripetere operazione
+
+*/
+
+
+// Chiedo mail utente
+const mailUtente = prompt ("Inserisci tuo indirizzo e-mail per accedere");
+
+//Lista mail autorizzate
+const mailAutorizzate = ["utente1@boolean.it", "utente2@boolean.it", "utente3@boolean.it", "utente4@boolean.it"]
+
+//Controllo presenza mail utente nella lista mail autorizzate
+
+for (let i = 0; i < mailAutorizzate.length; i++) {
+    if (mailUtente == mailAutorizzate[i]) {
+        console.log ("Email corretta")
+    } 
+}
+
+
+    
+
+    
